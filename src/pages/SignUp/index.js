4def7f02845159dd1b34163ff403629bb1e1e6dd
ThiagoTeixeiraa <thiagosousateixeiraa@gmail.com>
2019-09-28 17:32:51 -0300

    estilização das telas de autenticação

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/">Já tenho uma Conta</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,6 +1,64 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(-90deg, #1f2386, #454bb7);
+  background: linear-gradient(-90deg, #2f0669, #4235b5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 15px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #6d12e3;
+      font-weight: bold;
+      color: #fff;
+      border-radius: 4px;
+      font-size: 16px;
+      border: 0px;
+      transition: all 0.2s;
+
+      &:hover {
+        background: ${darken(0.05, '#6d12e3')};
+      }
+    }
+
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+      transition: all 0.2s;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
 `;
