b3a10e6a9c0175c986eb79f0ec54a4785019c499
ThiagoTeixeiraa <thiagosousateixeiraa@gmail.com>
2019-09-29 19:35:02 -0300

    notificações

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-90deg, #2f0669, #4235b5);
+  /* background: #eeeeee; */
 `;
