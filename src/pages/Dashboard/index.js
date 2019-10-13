17e9e8745485a3b17d21f170946c86bf19836534
ThiagoTeixeiraa <thiagosousateixeiraa@gmail.com>
2019-10-13 13:23:32 -0300

    estilização da dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft color="#fff" size={36} />
+        </button>
+        <strong>13 de Outubro</strong>
+        <button type="button">
+          <MdChevronRight color="#fff" size={36} />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Thiago Teixeira</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Thiago Teixeira</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Thiago Teixeira</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
